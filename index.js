9b7897ed30089752fdce41ea1ce34f70b7ab33d7
Melvin <112740634+Zenosite@users.noreply.github.com>
2023-04-08 16:12:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -320,7 +320,7 @@ animate()
 setTimeout(() =>{
     Timer_status = "Pause"
     game_status = "Done"
-}, 1000)
+}, 100)
 function animate(){
     if(timer!=0 && game_status != "Done" && Timer_status != "Pause"){
         window.requestAnimationFrame(animate)
